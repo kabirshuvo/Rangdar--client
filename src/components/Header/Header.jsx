ec9cc72f4b3fd46b2c5c561feee14a35504581e7
kabir shuvo <kabirshuvo19@gmail.com>
2023-08-02 22:29:33 +0600

    Navber Logo imported and hover effect implimented

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import {logo} from '../../assets/index.js'
+const Header = () => {
+  return (
+    <div className='w-full bg-black text-white px-4 py-1'>
+        
+{/* image statrt == logo */}
+<div className='px-2 h-[80%] flex items-center border border-transparent hover:border-white cursor-pointer duration-100'>
+    <img className='w-24 mt-1' src={logo} alt="Rangdar Logo" />
+</div>
+{/* logo image ends */}
+{/* Deliver statrt */}
+
+{/* Deliver Ends */}
+{/* Search statrt */}
+
+{/* Search Ends */}
+{/* SignIn statrt */}
+
+{/* SignIn Ends */}
+{/* Orders statrt */}
+
+{/* Orders Ends */}
+{/* Cart statrt */}
+
+{/* Cart statrt */}
+
+</div>
+  )
+}
+
+export default Header
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,26 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      maxWidth:{
+        container: '1440px'
+      },
+      screens: {
+        xs: '320',
+        sm: '375',
+        sml:'500px',
+        md: '667px',
+        mdl:'768px',
+        lg: '960px',
+        lgl:'1024px',
+        xl: '1280px',
+        xxl:'1536px',
+      },
+      fontFamily:{
+        titleFont: 'Roboto',
+        bodyFont: 'Poppins',
+      },
+    },
   },
   plugins: [],
 }
